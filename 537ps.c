0087c6828f7a006d59905f66dacecd92c10f7024
Matthew Derzay <derzay@rockhopper-06.cs.wisc.edu>
2018-09-21 22:43:33 -0500

    Adding initial file

---
+++ 537ps.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	int opt;
+	DIR* procDirectory = opendir("/proc");
+
+	while((opt = getopt(argc, argv, "psUSvc:")) != -1) {
+		switch(opt) {
+		case 'p':
+			printf("p");
+			break;
+		case 's':
+			printf("s");
+			break;
+		case 'U':
+			printf("U");
+			break;
+		case 'S':
+			printf("S");
+			break;
+		case 'v':
+			printf("v");
+			break;
+		case 'c':
+			printf("c");
+			break;
+		default: 
+			printf("You fucked up.");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
