44c1773740b46549c0a0931f2f34a1360c601587
Matthew Derzay <derzay@rockhopper-04.cs.wisc.edu>
2018-09-23 20:34:52 -0500

    fix getopt

--- 537ps.c
+++ 537ps.c
@@ -9,29 +9,45 @@
 int main(int argc, char *argv[]) {
 	int opt;
 
-	while((opt = getopt(argc, argv, "psUSvc:")) != -1) {
+	while((opt = getopt(argc, argv, "p:s::U::S::v::c::")) != -1) {
 		switch(opt) {
 		case 'p':
-			printf("p");
-			printf(argv[optind]);
+			//if no -p print all
+			//otherwise print specific pid
+			printf("p %s\n", optarg);	
 			break;
 		case 's':
-			printf("s");
+			//do not print if no argument
+			//print if -s
+			//-s- is valid but no effect
+			printf("s %c\n", *optarg);
 			break;
 		case 'U':
-			printf("U");
+			//print if no argument
+			//do not print if -U-
+			//does not state what to do for -U
+			printf("U %c\n", *optarg);
 			break;
 		case 'S':
-			printf("S");
+			//do not print if no argument
+			//print if -S
+			//-S- is valid but no effect
+			printf("S %c\n", *optarg);
 			break;
 		case 'v':
-			printf("v");
+			//do not print if no argument
+			//print if -v
+			//-v- is valid but no effect
+			printf("v %c\n", *optarg);
 			break;
 		case 'c':
-			printf("c");
+			//print if no argument
+			//do not print if -c-
+			//does not state what to do for -c
+			printf("c %c\n", *optarg);
 			break;
 		default: 
-			printf("You fucked up.");
+			printf("You fucked up.\n");
 			exit(EXIT_FAILURE);
 		}
 	}
