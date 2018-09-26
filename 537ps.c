06401864e3824777d3c430b54fa731cc07d09a12
Matthew Derzay <derzay@emperor-05.cs.wisc.edu>
2018-09-25 21:47:47 -0500

    Added helper include statements

--- 537ps.c
+++ 537ps.c
@@ -6,6 +6,10 @@
 #include <errno.h>
 #include <string.h>
 #include "sHelper.h"
+#include "UHelper.h"
+#include "SHelper.h"
+#include "vHelper.h"
+#include "cHelper.h"
 
 
 char[] getInfo(int p,int s,int U,int S, int v, int c,char pid ){
