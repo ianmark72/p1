4114714cc97587a809f5e3feaf3544d7a7dc6989
Matthew Derzay <derzay@emperor-05.cs.wisc.edu>
2018-09-25 21:03:14 -0500

    Adding the rest of the helper functions

---
+++ SHelper.c
@@ -0,0 +1,35 @@
+#include "SHelper.h"
+
+char[] SHelper(char[] pid) {
+        //Create file object.
+        FILE *file;
+        //Start of the filepath.
+        char[] filePath = "/proc/;
+        char next[100];
+        int infoLine = 0;
+
+        //Concatenate filepath with pid and status file to get full filepath.
+        strncat(filePath, pid, 20);
+        strncat(filePath, "/stat", 30);
+
+        //Open file, r means read only.
+        file = fopen(filePath, "r");
+        //Check if the file is open.
+        if(file == NULL) {
+                printf("File cannot be opened\n");
+                exit(0);
+        }
+
+        while(fscanf(file, "%s", next) != EOF) {
+                if(infoLine == 2) {
+                        break;
+                }
+
+                infoLine++;
+        }
+
+        fclose(file);
+
+        return next;
+}
+
---
+++ vHelper.h
@@ -0,0 +1,2 @@
+char[] vHelper(char[]);
+
