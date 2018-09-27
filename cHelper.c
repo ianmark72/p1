7bec716056de262c4d40854e5f736532b3bee1d7
Ian Mark <imark@royal-07.cs.wisc.edu>
2018-09-27 01:16:18 -0500

    what is happening

--- cHelper.c
+++ cHelper.c
@@ -4,18 +4,19 @@
 #include <string.h>
 #include <errno.h>
 #include <ctype.h>
+#define BUF_SIZE_BIG 1024
+#define BUF_SIZE_SMALL 256
 
 void cHelper(char* pid, char* result) {
         //Create file object.
         FILE *file;
         //Start of the filepath.
         char filePath[] = "/proc/";
-        //char nextWord[100];
-	char finalOutput[200];
+	char finalOutput[BUF_SIZE_BIG];
 
         //Concatenate filepath with pid and status file to get full filepath.
-        strncat(filePath, pid, 20);
-        strncat(filePath, "/cmdline", 30);
+        strncat(filePath, pid, BUF_SIZE_SMALL);
+        strncat(filePath, "/cmdline", BUF_SIZE_SMALL);
 
         //Open file, r means read only.
         file = fopen(filePath, "r");
@@ -24,22 +25,22 @@ void cHelper(char* pid, char* result) {
                 printf("File cannot be opened\n");
                 exit(0);
         }
-	strncat(finalOutput, "[", 200);
+	strncat(finalOutput, "[", BUF_SIZE_BIG);
 	 do {
 		 char c;
                 if(( c = fgetc(file) ) == EOF ) { break; }
                 if ( c == '0' ) { /* add space to results */ 
-			       strncat(finalOutput, " ", 200);
+			       strncat(finalOutput, " ", BUF_SIZE_BIG);
 		}
 		else { /* add char to results */
 			char tmp[2];
 		       	tmp[0]	= c;
 			tmp[1] = '\0';
-			strncat(finalOutput, tmp , 200);       
+			strncat(finalOutput, tmp , BUF_SIZE_BIG);       
 		}
 
 } while(1);
-strncat(finalOutput, "]", 200);
+strncat(finalOutput, "]", BUF_SIZE_BIG);
        // while(fscanf(file, "%s", nextWord) != EOF) {
 	//	strncat(finalOutput, "[", 200);
         //	strncat(finalOutput, nextWord, 200);
